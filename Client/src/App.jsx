6507c3ba4318a334dc1be0786724bb62e46bfd5a
Nikhil Thakur <nikhil16thakur@gmail.com>
2024-10-23 19:20:20 +0530

    added shadcn ui

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+      <Button className="mt-10 bg-red-600"> Click me</Button>
+          
+    </>
+  )
+}
+
+export default App
