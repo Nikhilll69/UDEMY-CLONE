18344d783a26cb961096a7d91c987d313d8da3f9
Nikhil Thakur <nikhil16thakur@gmail.com>
2024-10-26 17:41:05 +0530

    added tabs using shdcn ui

--- Client/src/pages/auth/index.jsx
+++ Client/src/pages/auth/index.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+import { GraduationCap } from 'lucide-react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function AuthPage() {
+  const [activeTab, setActiveTab] = useState("signin")
+
+  const handleTabChange = (value) => {
+    setActiveTab(value)
+
+  }
   return (
-    <div>AuthPage</div>
+
+    <div className='flex flex-col bg-zinc-100 min-h-screen'>
+      <header className='px-4 lg:px-6 h-14 flex items-center border-b'>
+        <Link className='flex items-center justify-center' to={'/'} >
+        </Link>
+        <GraduationCap className='h-8 w-8 mr-4' />
+        <span className='font-extrabold text-xl' >LMS LEARN</span>
+      </header>
+      <div className='flex items-center justify-center min-h-screen bg-background'>
+        <Tabs value={activeTab} defaultValue="signin" onValueChange={handleTabChange} className='w-full max-w-md'>
+
+          <TabsList className='grid w-full grid-cols-2'>
+            <TabsTrigger value='signin'>Sign In</TabsTrigger>
+            <TabsTrigger value='signup'>Sign Up</TabsTrigger>
+          </TabsList>
+          <TabsContent value='signin'>sign In</TabsContent>
+          <TabsContent value='signup'>sign up</TabsContent>
+
+        </Tabs>
+      </div>
+    </div>
   )
+
 }
 
 export default AuthPage
